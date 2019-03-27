065d2a6df56ff02db8d592e08da06d3c6d3f32bb
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-27 16:38:08 -0600

    all crud ops should be working.

--- routers/cohorts.js
+++ routers/cohorts.js
@@ -7,13 +7,38 @@ const db = knex(dbConfig.development);
 
 router.use(express.json())
 
+router.post('/', async (req, res) => {
+    try {
+      const [id] = await db('cohorts').insert(req.body);
+  
+      const cohorts = await db('cohorts')
+        .where({ id })
+        .first();
+  
+      res.status(201).json(cohorts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
 router.get('/', async (req, res) => {
     try {
-        const cohorts = await db('cohorts'); // all the records from the table
-        res.status(200).json(cohorts);
+      const cohorts = await db('cohorts');
+      res.status(200).json(cohorts);
     } catch (error) {
-        res.status(500).json(error);
+      res.status(500).json(error);
     }
-})
+  });
+
+router.get('/:id', async (req, res) => {
+    try {
+      const cohorts = await db('cohorts')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(cohorts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 module.exports = router;
--- routers/students.js
+++ routers/students.js
@@ -9,11 +9,43 @@ router.use(express.json())
 
 router.get('/', async (req, res) => {
     try {
-        const students = await db('students'); // all the records from the table
-        res.status(200).json(students);
+      const students = await db('students')
+      res.status(200).json(students);
     } catch (error) {
-        res.status(500).json(error);
+      res.status(500).json(error);
     }
-})
+  });
+
+router.put('/:id', async (req, res) => {
+    try {
+      const count = await db('students')
+        .where({ id: req.params.id })
+        .update(req.body);
+  
+      if (count > 0) {
+        const students = await db('students')
+          .where({ id: req.params.id })
+          .first();
+
+        res.status(200).json(students);
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const count = await db('students')
+        .where({ id: req.params.id })
+        .del();
+  
+      if (count > 0) {
+        res.status(204).json(count);
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });
 
 module.exports = router;
